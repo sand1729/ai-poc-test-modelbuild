65bed2abcce1df1619591779313e24ea56b82e0b
Sandeep Reddy Lekkala <slekkala@velocityclinical.com>
2023-03-23 15:46:57 +0530

    changed to iris

--- pipelines/iris/preprocess.py
+++ pipelines/iris/preprocess.py
@@ -1,4 +1,4 @@
-"""Feature engineers the abalone dataset."""
+"""Feature engineers the iris dataset."""
 import argparse
 import logging
 import os
@@ -22,35 +22,29 @@ logger.addHandler(logging.StreamHandler())
 
 # Since we get a headerless CSV file we specify the column names here.
 feature_columns_names = [
-    "sex",
-    "length",
-    "diameter",
-    "height",
-    "whole_weight",
-    "shucked_weight",
-    "viscera_weight",
-    "shell_weight",
+    "Sepal length",
+    "Sepal width",
+    "Petal length",
+    "Petal width",
+    "Species"
 ]
-label_column = "rings"
+# label_column = "Species"
 
 feature_columns_dtype = {
-    "sex": str,
-    "length": np.float64,
-    "diameter": np.float64,
-    "height": np.float64,
-    "whole_weight": np.float64,
-    "shucked_weight": np.float64,
-    "viscera_weight": np.float64,
-    "shell_weight": np.float64,
+    "Sepal length": np.float64,
+   "Sepal width": np.float64,
+    "Petal length": np.float64,
+    "Petal width": np.float64,
+    "Species": str
 }
-label_column_dtype = {"rings": np.float64}
+# label_column_dtype = {"Species": np.str}
 
 
-def merge_two_dicts(x, y):
-    """Merges two dicts, returning a new copy."""
-    z = x.copy()
-    z.update(y)
-    return z
+# def merge_two_dicts(x, y):
+#     """Merges two dicts, returning a new copy."""
+#     z = x.copy()
+#     z.update(y)
+#     return z
 
 
 if __name__ == "__main__":
@@ -66,7 +60,7 @@ if __name__ == "__main__":
     key = "/".join(input_data.split("/")[3:])
 
     logger.info("Downloading data from bucket: %s, key: %s", bucket, key)
-    fn = f"{base_dir}/data/abalone-dataset.csv"
+    fn = f"{base_dir}/data/iris_data.csv"
     s3 = boto3.resource("s3")
     s3.Bucket(bucket).download_file(key, fn)
 
@@ -74,19 +68,19 @@ if __name__ == "__main__":
     df = pd.read_csv(
         fn,
         header=None,
-        names=feature_columns_names + [label_column],
-        dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype),
+        names=feature_columns_names ,
+        dtype= feature_columns_dtype,
     )
     os.unlink(fn)
 
     logger.debug("Defining transformers.")
     numeric_features = list(feature_columns_names)
-    numeric_features.remove("sex")
+    numeric_features.remove("Species")
     numeric_transformer = Pipeline(
         steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
     )
 
-    categorical_features = ["sex"]
+    categorical_features = ["Species"]
     categorical_transformer = Pipeline(
         steps=[
             ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
@@ -102,7 +96,7 @@ if __name__ == "__main__":
     )
 
     logger.info("Applying transforms.")
-    y = df.pop("rings")
+    y = df.pop("Species")
     X_pre = preprocess.fit_transform(df)
     y_pre = y.to_numpy().reshape(len(y), 1)
 
